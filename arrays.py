919dcfdd4d85ffef144018f08fb70b53b576ea13
420-Paliwal <harshpaliwal0420@gmail.com>
2026-05-07 05:09:07 +0530

    remove duplicate from array

--- arrays.py
+++ arrays.py
@@ -3,3 +3,12 @@ def isSorted(arr):
         if arr[i] < arr[i - 1]:
             return False
     return True
+
+
+def removeduplicate(arr):
+    i = 0 
+    for j in range(1, len(arr)):
+        if arr[j] != arr[i]:
+            i += 1
+            arr[i] = arr[j]
+    return arr
