2037afd7e9571e76e26f9cb0f26cfb7f0cd7501d
420-Paliwal <harshpaliwal0420@gmail.com>
2026-05-07 05:19:43 +0530

    reotate array by one

--- arrays.py
+++ arrays.py
@@ -12,3 +12,14 @@ def removeduplicate(arr):
             i += 1
             arr[i] = arr[j]
     return arr
+
+def rotateArray(arr):
+    temp = arr[0]
+    n = len(arr)
+    for i in range(n - 1):
+        arr[i] = arr[i+1]
+    arr[n - 1] = temp
+    return arr
+
+arr = [1, 2, 3, 4, 5]
+print(rotateArray(arr))
